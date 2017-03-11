18beacf1de1226b4b82dabb650a40819fd58aacd
rpi_home_security <darteaga08@gmail.com>
2017-03-11 18:42:34 -0500

    Small bug
    Small bug

--- start_motion.sh
+++ start_motion.sh
@@ -7,4 +7,4 @@
 # Change to the Desktop directory
 cd /home/pi/Desktop
 
-/usr/bin/wget -q -O /dev/null "192.168.1.110:8086/0/detection/start" 
+/usr/bin/wget -q -O /dev/null "192.168.1.110:8086/0/detection/start"
